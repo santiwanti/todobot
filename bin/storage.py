ba155f6fbdfdfec066a1fc582d0759e21c808b24
theHoatzin <santi.dtord@gmail.com>
2018-02-18 19:22:03 +0100

    Created Storage Exception and cleaned the code up a bit.

--- bin/storage.py
+++ bin/storage.py
@@ -3,6 +3,12 @@ from bin.todo import Todo
 _filepath = "../lists/"
 
 
+class StorageError(KeyError):
+    def __init__(self, message, errors=None):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message, errors)
+
+
 def _separate_first_word(line):
     separated_str = line.split(' ', 1)
     return separated_str[1], separated_str[0]
@@ -13,6 +19,24 @@ def _get_id(line):
     return separated_line, int(todo_id)
 
 
+def _contains_id(filename, expected_id):
+    with open(filename, 'a') as f:
+        for line in f:
+            line, todo_id = _get_id(line)
+            if todo_id == expected_id:
+                return True
+    return False
+
+
+def _remove_id(filename, todo_id):
+    output = []
+    with open(filename, 'r') as f:
+        for line in f:
+            if todo_id != _get_id(line):
+                output.append(line)
+    return output
+
+
 class Storage(object):
         
     def __init__(self, filename):
@@ -21,6 +45,8 @@ class Storage(object):
     def store_todo(self, todo):
         if not isinstance(todo, Todo):
             raise TypeError("%s attribute must be set to an instance of %s" % (todo, Todo))
+        if _contains_id(self._filename, todo.id):
+            raise StorageError("This id is already in use.")
         with open(self._filename, 'a') as f:
             text = str(todo.id) + ' ' + todo.description + '\n'
             f.write(text)
@@ -35,12 +61,8 @@ class Storage(object):
         return todos
     
     def delete_todo(self, todo_id):
-        f = open(self._filename)
-        output = []
-        for line in f:
-            if todo_id != line.split(' ', 1)[0]:
-                output.append(line)
-        f.close()
-        f = open(self._filename, 'w')
-        f.writelines(output)
-        f.close()
+        if not _contains_id(self._filename, todo_id):
+            raise StorageError("This id does not exist")
+        remaining_lines = _remove_id(self._filename, todo_id)
+        with open(self._filename, 'w') as f:
+            f.writelines(remaining_lines)
