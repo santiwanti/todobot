8d11956e55328307b799794d80736f80abf65706
theHoatzin <santi.dtord@gmail.com>
2018-02-21 23:57:38 +0100

    Regexp are used to detect TODOs meaning TOBUY can also be used. Each type of TODO is kept in a different filename meaning TODOs and TOBUYs are listed separately.

--- bin/bot-interaction.py
+++ bin/bot-interaction.py
@@ -23,8 +23,11 @@ def get_todo_id():
     return randint(0, 1000)
 
 
-def get_filename(message: types.Message):
-    return str(message.chat.id) + '.td'
+def get_filename(message: types.Message, end_data: str):
+    chat_id = str(message.chat.id)
+    cut_end = len(end_data)
+    todo_type = str(message.get_command()[3:-cut_end])
+    return chat_id + '_' + todo_type + '.td'
 
 
 @dp.message_handler(commands=['start'])
@@ -38,27 +41,35 @@ async def show_help(message: types.Message):
                         "tododone TODO_ID: deletes a TODO")
 
 
-@dp.message_handler(commands=['todoadd', 'Todoadd'])
+@dp.message_handler(regexp='[tT]o.+add')
 async def save_todo(message: types.Message):
-    filename = get_filename(message)
+    filename = get_filename(message, "add")
     todo = Todo(get_todo_id(), message.get_args())
     Storage(filename).store_todo(todo)
 
 
-@dp.message_handler(commands=['tododone', 'Tododone'])
+@dp.message_handler(regexp='[tT]o.+done')
 async def todo_done(message: types.Message):
-    filename = get_filename(message)
+    filename = get_filename(message, "done")
     todo_id = message.get_args()
     Storage(filename).delete_todo(todo_id)
 
 
-@dp.message_handler(commands=['todolist', 'Todolist'])
+@dp.message_handler(regexp='[tT]o.+list')
 async def show_todo(message: types.Message):
-    txt = 'TODOs:\n'
-    filename = get_filename(message)
+    todo_type = str(message.get_command())[3:-4]
+    txt = 'TO-' + todo_type.upper() + 's:\n'
+    filename = get_filename(message, "list")
     todos = Storage(filename).get_todos()
     for todo in todos:
         txt += str(todo.id) + '. ' + todo.description
+    await message.reply(txt)
+
+
+@dp.message_handler(command=['list', 'List'])
+async def show_todo(message: types.Message):
+    txt = "Lists:\n"
+    txt += Storage.existing_lists(str(message.chat))
     await message.reply(txt)
 
 
